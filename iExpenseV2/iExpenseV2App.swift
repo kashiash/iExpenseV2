3052154df0b35a95a9c9bd080cab0d6643ecb36f
kashiash <kashiash@gmail.com>
2022-07-16 16:27:42 +0200

    Initial Commit

---
+++ iExpenseV2/iExpenseV2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseV2App.swift
+//  iExpenseV2
+//
+//  Created by Jacek Placek on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
